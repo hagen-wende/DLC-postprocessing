4a23d27b16331de3b7dfea927cebd753df64c61c
“Hagen <coding@hagen-wende.com>
2020-07-14 22:12:50 +0200

    bla

--- post-processing.py
+++ post-processing.py
@@ -11,15 +11,22 @@ os.chdir(path)
 Dataframe = pd.read_hdf(os.path.join("data","200711beetles_sample_sk.h5"))
 #
 
-df = Dataframe.iloc[:, Dataframe.columns.get_level_values(1)=='paemula5']
+df = Dataframe.iloc[:, Dataframe.columns.get_level_values(1)=='paemula8']
 df = df.iloc[:, df.columns.get_level_values(2)=='scutellum']
+df = Dataframe.iloc[:, Dataframe.columns.get_level_values(2)=='scutellum']
 
+df.plot(legend=False)
 df.columns = [col[3] for col in df.columns]
 
 # colors
 cmap = plt.cm.rainbow
-norm = matplotlib.colors.Normalize(vmin=1, vmax=1600)
+norm = matplotlib.colors.Normalize(vmin=1, vmax=8)
 
 plt.scatter('x','y', data=df.iloc[:,[0,1]], color=cmap(norm(df.index.values)))
+for i, coordinates in enumerate(set(df.columns.get_level_values(1))):
+    df.iloc[:, df.columns.get_level_values(1)==coordinates]
+    plt.scatter('x','y', data=df.iloc[:, df.columns.get_level_values(1)==coordinates].iloc[:,[0,1]], color=cmap(norm(i)))
+
+plt.hist('x',data=df.iloc[:,[0]])
 
 #plt.show()
