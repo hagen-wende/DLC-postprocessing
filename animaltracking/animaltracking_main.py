dfc03ad94a28640b9c0ee7c936aa3c1deb8b81de
“Hagen <coding@hagen-wende.com>
2020-07-24 12:47:38 +0200

    edited code for presentation videos

--- animaltracking/animaltracking_main.py
+++ animaltracking/animaltracking_main.py
@@ -101,6 +101,8 @@ for video in projectfiles['videos']:
     if h5_file:
         projectfiles['h5files'][projectfiles.index[projectfiles['videos'] == video]] = h5_file[0]
 
+
+
 ###################
 #### Analysis #####
 # general activity
@@ -115,7 +117,7 @@ starttime = 8 # starttime of video in hours
 
 for h5file in projectfiles['h5files']:
     if h5file:
-        foodcsv = projectfiles['foodcsvs'][projectfiles.index[projectfiles['h5files'] == h5file]][0]
+        foodcsv = projectfiles['foodcsvs'][projectfiles.index[projectfiles['h5files'] == h5file]].values[0]
 
         # feedingcount.feeding(h5file, csv of food cirlce, fps)
         animalsfeeding = analysis.feeding(h5file, foodcsv, 2)
@@ -123,10 +125,10 @@ for h5file in projectfiles['h5files']:
         animalsactive = analysis.activity(h5file, 2)
 
         combined_df = pd.concat([animalsfeeding, animalsactive])
+        #plotactivity graph
         plotactivity(h5file, combined_df, rollingwindow, starttime)
     else:
         print("no data to analyse for ", projectfiles['videos'][projectfiles.index[projectfiles['h5files'] == h5_file]][0])
-
 
 
 
@@ -145,13 +147,18 @@ import cv2
 import csv
 import numpy as np
 
+RGBblack = (0,0,0)
+RGBwhite = (255,255,255)
+
+bgcolor = RGBblack
+
 stillframe = projectfiles['stillframes'][projectfiles.index[projectfiles['h5files'] == h5file]][0]
 
 image = cv2.imread(stillframe)
 print(image.shape)
 (im_height, im_width, color) = image.shape
 image = np.zeros((im_height,im_width,color), np.uint8)
-image[:,:]=(255,255,255)
+image[:,:]=bgcolor
 
 df = animalsfeeding
 df = df.iloc[:, df.columns.get_level_values(1).isin(['paemula3'])]
@@ -163,7 +170,7 @@ df = df.iloc[:, (df.columns.get_level_values('bodyparts') == 'head')]
 vidpath, vidname = os.path.split(os.path.abspath(projectfiles['videos'][projectfiles.index[projectfiles['h5files'] == h5_file]][0]))
 
 # output video
-vidout = cv2.VideoWriter('../data/output/project_white.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 25, (im_width,im_height))
+vidout = cv2.VideoWriter('../data_vis/output/project_black.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 25, (im_width,im_height))
 
 food = []
 # get circle coordinates
@@ -178,14 +185,15 @@ if os.path.isfile(projectfiles['foodcsvs'][projectfiles.index[projectfiles['h5fi
             food += [[x,y,rad]]
 
 feeding = []
+
 df.columns = [col[3] for col in df.columns]
 # black frames for the first frames in which the trail doesn't fit
 for frame in range(trail):
-    image[:,:]=(255,255,255)
+    image[:,:]=bgcolor
     vidout.write(image)
 
 for frame in range(trail, len(df)):
-    image[:,:]=(255,255,255)
+    image[:,:]=bgcolor
 
     for x,y,time in zip(list(df[frame-trail:frame]['x']),list(df[frame-trail:frame]['y']), [(x-max(list(df[frame-trail:frame].index)))/2 for x in list(df[frame-trail:frame].index)]):
         if not (math.isnan(x) & math.isnan(y)):
@@ -195,16 +203,19 @@ for frame in range(trail, len(df)):
             pointcolor=tuple([int(x*256) for x in cmap(norm(time))[:3]])
             image = cv2.circle(image, (x,y), radius=5, color=pointcolor, thickness=-1)
 
-    if (df['sumfeeding_0'][frame] == 1) | (df['sumfeeding_1'][frame] == 1):
-        feeding += [[int(df['x'][frame]), int(df['y'][frame])]]
+    ### skip a few for presentation vid
+    if frame > 300:
+        if (df['sumfeeding_0'][frame] == 1) | (df['sumfeeding_1'][frame] == 1):
+            feeding += [[int(df['x'][frame]), int(df['y'][frame])]]
+
 
     # draw green circles for visits
     for coordinate in feeding:
         image = cv2.circle(image, tuple(coordinate), radius=5, color=(0,255,0), thickness=-1)
 
     #draw food circles
-    image = cv2.circle(image, (food[0][0],food[0][1]), radius=food[0][2], color=(0,0,0), thickness=2)
-    image = cv2.circle(image, (food[1][0],food[1][1]), radius=food[1][2], color=(0,0,0), thickness=2)
+    image = cv2.circle(image, (food[0][0],food[0][1]), radius=food[0][2], color=RGBblack, thickness=2)
+    image = cv2.circle(image, (food[1][0],food[1][1]), radius=food[1][2], color=RGBblack, thickness=2)
 
     vidout.write(image)
 
